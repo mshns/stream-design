22f4c87f8f2bd58209fc22b48e842078dd96445b
mshns <mishanos412@gmail.com>
2023-09-29 16:12:17 +0300

    feat: add subtitles in badge

--- src/widgets/Badge/Badge.tsx
+++ src/widgets/Badge/Badge.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
+
+import { SUBTITLE_LIST } from './constants/subtitleList';
 import styles from './Badge.module.scss';
 
 export const Badge = () => {
+  const [subtitle, setSubtitle] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSubtitle((prev) => (prev === SUBTITLE_LIST.length - 1 ? 0 : prev + 1));
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className={styles.badge}>
       <h3 className={styles.title}>Александр 'storo08'</h3>
-      <h4 className={styles.subtitle}>Профессиональный игрок в покер</h4>
+      <h4 className={styles.subtitle}>{SUBTITLE_LIST[subtitle]}</h4>
     </div>
   );
 };
